df0b9ea0eaa49d8f0ccae0a3385cc67dd23930c1
koros <korosov.j@gmail.com>
2022-10-09 17:05:50 +0300

    FCOMM

--- StudiaKuhon/StudiaKuhon/settings.py
+++ StudiaKuhon/StudiaKuhon/settings.py
@@ -73,7 +73,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'StudiaKuhon',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': '0803',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
